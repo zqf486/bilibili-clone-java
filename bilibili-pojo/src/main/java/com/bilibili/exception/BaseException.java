6d085f600d5c5b75ed3e1724ea30d3e112eac635
zqf486 <231123124@stu.pxu.edu.cn>
2026-03-21 14:51:06 +0800

    功能实现：     添加软件包 exception

---
+++ bilibili-pojo/src/main/java/com/bilibili/exception/BaseException.java
@@ -0,0 +1,7 @@
+package com.bilibili.exception;
+
+public class BaseException extends RuntimeException {
+    public BaseException(String message) {
+        super(message);
+    }
+}
---
+++ bilibili-pojo/src/main/java/com/bilibili/exception/BusinessException.java
@@ -0,0 +1,7 @@
+package com.bilibili.exception;
+
+public class BusinessException extends BaseException {
+    public BusinessException(String message) {
+        super(message);
+    }
+}
