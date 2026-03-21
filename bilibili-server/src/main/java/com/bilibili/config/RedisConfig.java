d5913ffd16024650474abc0cff6b3e42bd1a3448
zqf486 <231123124@stu.pxu.edu.cn>
2026-03-21 14:03:28 +0800

    功能实现：     RedisConfig 配置序列化与反序列化器

---
+++ bilibili-server/src/main/java/com/bilibili/config/RedisConfig.java
@@ -0,0 +1,40 @@
+package com.bilibili.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig {
+
+    /**
+     * 配置redis 序列化反序列化
+     *
+     * @param factory
+     * @return
+     */
+    @Bean("redisTemplate")
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory factory) {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(factory);
+
+        // key 用字符串
+        StringRedisSerializer stringRedisSerializer = new StringRedisSerializer();
+
+        // value 用 json
+        GenericJackson2JsonRedisSerializer jsonRedisSerializer = new GenericJackson2JsonRedisSerializer();
+
+        template.setKeySerializer(stringRedisSerializer);
+        template.setValueSerializer(jsonRedisSerializer);
+
+        template.setHashKeySerializer(stringRedisSerializer);
+        template.setHashValueSerializer(jsonRedisSerializer);
+
+        template.afterPropertiesSet();
+
+        return template;
+    }
+}
