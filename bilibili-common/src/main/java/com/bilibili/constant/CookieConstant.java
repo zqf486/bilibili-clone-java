76e2fb948376ce62d1a7b625adf6ff0f0f7dc95e
zqf486 <231123124@stu.pxu.edu.cn>
2026-03-23 09:23:35 +0800

    功能实现:     CookieUtil set clean

---
+++ bilibili-common/src/main/java/com/bilibili/constant/CookieConstant.java
@@ -0,0 +1,12 @@
+package com.bilibili.constant;
+
+public class CookieConstant {
+    /**
+     * cookie中token的key
+     */
+    public static final String TOKEN_KEY = "token";
+    /**
+     * cookie过期时间 单位(s)
+     */
+    public static final int COOKIE_AGE = 60 * 60 * 24 * 7;
+}
---
+++ bilibili-common/src/main/java/com/bilibili/util/CookieUtil.java
@@ -0,0 +1,32 @@
+package com.bilibili.util;
+
+import com.bilibili.constant.CookieConstant;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class CookieUtil {
+
+    /**
+     * 保存 token 到 Cookie
+     *
+     * @param response
+     */
+    public static void saveToken2Cookie(HttpServletResponse response, String token) {
+        Cookie cookie = new Cookie(CookieConstant.TOKEN_KEY, token);
+        cookie.setMaxAge(CookieConstant.COOKIE_AGE);
+        cookie.setPath("/");
+        response.addCookie(cookie);
+    }
+
+    /**
+     * 清除前端 cookie 中的 token
+     *
+     * @param response
+     */
+    public static void cleanToken2Cookie(HttpServletResponse response) {
+        Cookie cookie = new Cookie(CookieConstant.TOKEN_KEY, null);
+        cookie.setMaxAge(0);
+        cookie.setPath("/");
+        response.addCookie(cookie);
+    }
+}
