0e424649237bd484131b48adb7ca3a9db253e82f
zqf486 <231123124@stu.pxu.edu.cn>
2026-03-25 14:45:30 +0800

    RedisConstant字段更新

--- bilibili-common/src/main/java/com/bilibili/constant/RedisConstant.java
+++ bilibili-common/src/main/java/com/bilibili/constant/RedisConstant.java
@@ -13,4 +13,12 @@ public class RedisConstant {
      * 过期时间： 60s
      */
     public static final long REDIS_KEY_EXPIRES_ONE_MIN = 60;
+    /**
+     * 过期时间： 1 day
+     */
+    public static final long REDIS_KEY_EXPIRES_ONE_DAY = 60 * 60 * 24;
+    /**
+     * web端token
+     */
+    public static final String REDIS_TOKEN_KEY_WEB = REDIS_KEY_PREFIX + "token:web:";
 }
