25a1fe44f820cb485ec15f4f177dc2a09fc66d6a
zqf486 <231123124@stu.pxu.edu.cn>
2026-03-31 11:08:23 +0800

    业务逻辑修改:     新增token cookie 刷新拦截器, 拦截所有请求并刷新请求中的token有效期     修改login 拦截器, 只判断当前线程上下文是否含有UserLoginVO

--- bilibili-server/src/main/java/com/bilibili/config/WebMvcConfig.java
+++ bilibili-server/src/main/java/com/bilibili/config/WebMvcConfig.java
@@ -1,6 +1,7 @@
 package com.bilibili.config;
 
 import com.bilibili.interceptor.LoginInterceptor;
+import com.bilibili.interceptor.RefreshTokenInterceptor;
 import jakarta.annotation.Resource;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -12,14 +13,24 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Resource
     private LoginInterceptor loginInterceptor;
 
+    @Resource
+    private RefreshTokenInterceptor refreshTokenInterceptor;
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
 
         /**
+         * token cookie 刷新拦截器
+         */
+        registry.addInterceptor(refreshTokenInterceptor)
+                .addPathPatterns("/api/**")
+                .order(0);
+
+        /**
          * 用户登录拦截器
          */
         registry.addInterceptor(loginInterceptor)
-                .addPathPatterns("/api/**");
+                .addPathPatterns("/api/**")
+                .order(1);
     }
 }
---
+++ bilibili-server/src/main/java/com/bilibili/interceptor/RefreshTokenInterceptor.java
@@ -0,0 +1,101 @@
+package com.bilibili.interceptor;
+
+import com.bilibili.constant.CookieConstant;
+import com.bilibili.constant.RedisConstant;
+import com.bilibili.context.UserContext;
+import com.bilibili.util.RedisUtil;
+import com.bilibili.vo.UserLoginVO;
+import jakarta.annotation.Resource;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.lang.Nullable;
+import org.springframework.stereotype.Component;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+@Slf4j
+@Component
+public class RefreshTokenInterceptor implements HandlerInterceptor {
+
+    @Resource
+    private RedisUtil redisUtil;
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+
+        // 0.非controller 请求直接放行
+        if (!(handler instanceof HandlerMethod)) {
+            return true;
+        }
+
+        // 1.获取cookie 中的 token
+        String token = getTokenFromCookie(request);
+
+        // 1.1. token不存在，直接放行
+        if (token == null) {
+            return true;
+        }
+
+        String key = RedisConstant.REDIS_TOKEN_KEY_WEB + token;
+        UserLoginVO user = redisUtil.get(key, UserLoginVO.class);
+
+        // 2. token 过期，直接放行
+        if (user == null) {
+            return true;
+        }
+
+        // 3. 存 ThreadLocal
+        UserContext.set(user);
+
+        // 4.滑动过期
+        Long ttl = redisUtil.getExpire(key);
+        if (ttl != null && ttl > 0 && ttl < RedisConstant.REDIS_KEY_EXPIRES_ONE_DAY / 2) {
+            redisUtil.expire(key, RedisConstant.REDIS_KEY_EXPIRES_ONE_DAY);
+            // 4.1.同步刷新cookie
+            refreshCookie(response, token);
+        }
+
+        return true;
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, @Nullable Exception ex) throws Exception {
+        // 移除用户
+        UserContext.clear();
+    }
+
+    /**
+     * 从 request中拿去 cookie 并读取token
+     *
+     * @param request
+     * @return
+     */
+    private String getTokenFromCookie(HttpServletRequest request) {
+        if (request.getCookies() == null) {
+            return null;
+        }
+
+        for (Cookie cookie : request.getCookies()) {
+            if (CookieConstant.TOKEN_KEY.equals(cookie.getName())) {
+                return cookie.getValue();
+            }
+        }
+        return null;
+    }
+
+    /**
+     * 刷新 请求客户端的cookie过期时间
+     *
+     * @param response
+     * @param token
+     */
+    private void refreshCookie(HttpServletResponse response, String token) {
+        Cookie cookie = new Cookie(CookieConstant.TOKEN_KEY, token);
+        cookie.setMaxAge(CookieConstant.COOKIE_AGE);
+        cookie.setPath("/");
+        cookie.setHttpOnly(true);
+        response.addCookie(cookie);
+    }
+}
