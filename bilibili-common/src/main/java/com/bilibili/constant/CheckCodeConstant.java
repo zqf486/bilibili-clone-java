fc0516bb0a470c2ac41b47b82439f9aadd3b2290
zqf486 <231123124@stu.pxu.edu.cn>
2026-03-31 11:04:45 +0800

    字段修改：     将CheckCode WIDTH 150 -> 200

--- bilibili-common/src/main/java/com/bilibili/constant/CheckCodeConstant.java
+++ bilibili-common/src/main/java/com/bilibili/constant/CheckCodeConstant.java
@@ -2,6 +2,6 @@ package com.bilibili.constant;
 
 public class CheckCodeConstant {
 
-    public static final int WIDTH = 160;
+    public static final int WIDTH = 200;
     public static final int HEIGHT = 50;
 }
