5df19caf6e13b5e917d0d10a5a7c3bfde983bc1b
zqf486 <231123124@stu.pxu.edu.cn>
2026-03-31 11:08:44 +0800

    业务逻辑修改:     新增token cookie 刷新拦截器, 拦截所有请求并刷新请求中的token有效期     修改login 拦截器, 只判断当前线程上下文是否含有UserLoginVO

--- bilibili-server/src/main/java/com/bilibili/interceptor/LoginInterceptor.java
+++ bilibili-server/src/main/java/com/bilibili/interceptor/LoginInterceptor.java
@@ -1,15 +1,10 @@
 package com.bilibili.interceptor;
 
 import com.bilibili.annotation.LoginRequired;
-import com.bilibili.constant.CookieConstant;
 import com.bilibili.constant.MessageConstant;
-import com.bilibili.constant.RedisConstant;
 import com.bilibili.context.UserContext;
 import com.bilibili.exception.AuthException;
-import com.bilibili.util.RedisUtil;
 import com.bilibili.vo.UserLoginVO;
-import jakarta.annotation.Resource;
-import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
@@ -20,9 +15,6 @@ import org.springframework.web.servlet.HandlerInterceptor;
 @Slf4j
 @Component
 public class LoginInterceptor implements HandlerInterceptor {
-
-    @Resource
-    private RedisUtil redisUtil;
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
@@ -42,67 +34,13 @@ public class LoginInterceptor implements HandlerInterceptor {
             return true;
         }
 
-        // 2.获取 cookie中的token
-        String token = getTokenFromCookie(request);
-
-        if (token == null) {
-            throw new AuthException(MessageConstant.USER_NOT_LOGIN);
-        }
-
-        // 2.1.获取 redis 登录状态
-        String key = RedisConstant.REDIS_TOKEN_KEY_WEB + token;
-        UserLoginVO user = redisUtil.get(key, UserLoginVO.class);
+        // 2.获取 ThreadLocal 中的用户信息
+        UserLoginVO user = UserContext.get();
 
         if (user == null) {
             throw new AuthException(MessageConstant.USER_NOT_LOGIN);
         }
 
-        // 3.存 ThreadLocal
-        UserContext.set(user);
-
-        // 2.滑动过期
-        Long ttl = redisUtil.getExpire(key);
-        log.info("ttl: {}", ttl);
-        log.info("ttl2: {}", RedisConstant.REDIS_KEY_EXPIRES_ONE_DAY / 2);
-        if (ttl != null && ttl > 0 && ttl < RedisConstant.REDIS_KEY_EXPIRES_ONE_DAY / 2) {
-            redisUtil.expire(key, RedisConstant.REDIS_KEY_EXPIRES_ONE_DAY);
-            // 2.1.同步刷新cookie
-            refreshCookie(response, token);
-        }
-
         return true;
-    }
-
-    /**
-     * 从 request中拿去 cookie 并读取token
-     *
-     * @param request
-     * @return
-     */
-    private String getTokenFromCookie(HttpServletRequest request) {
-        if (request.getCookies() == null) {
-            return null;
-        }
-
-        for (Cookie cookie : request.getCookies()) {
-            if (CookieConstant.TOKEN_KEY.equals(cookie.getName())) {
-                return cookie.getValue();
-            }
-        }
-        return null;
-    }
-
-    /**
-     * 刷新 请求客户端的cookie过期时间
-     *
-     * @param response
-     * @param token
-     */
-    private void refreshCookie(HttpServletResponse response, String token) {
-        Cookie cookie = new Cookie(CookieConstant.TOKEN_KEY, token);
-        cookie.setMaxAge(CookieConstant.COOKIE_AGE);
-        cookie.setPath("/");
-        cookie.setHttpOnly(true);
-        response.addCookie(cookie);
     }
 }
