d0605546204d7eb2fadbea17de1f3962a5d19989
zqf486 <231123124@stu.pxu.edu.cn>
2026-04-28 17:58:46 +0800

    improvement: 重启项目会清除 CacheConstant.CATEGORY_TREE_KEY

--- bilibili-server/src/main/java/com/bilibili/init/cache/impl/CategoryCachePreloader.java
+++ bilibili-server/src/main/java/com/bilibili/init/cache/impl/CategoryCachePreloader.java
@@ -1,7 +1,9 @@
 package com.bilibili.init.cache.impl;
 
+import com.bilibili.constant.CacheConstant;
 import com.bilibili.init.cache.CachePreLoader;
 import com.bilibili.service.ICategoryService;
+import com.bilibili.util.RedisUtil;
 import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
@@ -13,9 +15,13 @@ public class CategoryCachePreloader implements CachePreLoader {
     @Resource
     private ICategoryService categoryService;
 
+    @Resource
+    private RedisUtil redisUtil;
+
     @Override
     public void preload() {
         log.info("开始重建分类缓存");
+        redisUtil.delete(CacheConstant.CATEGORY_TREE_KEY);
         categoryService.refreshCache();
         log.info("分类缓存重建完成");
     }
