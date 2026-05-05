398b47dbed8a5c3e0340470f3e4307ba362bb05c
zqf486 <231123124@stu.pxu.edu.cn>
2026-05-06 00:53:41 +0800

    feat: 添加上传任务和分集 Mapper，分片上传 DTO/VO

---
+++ bilibili-server/src/main/java/com/bilibili/mapper/UploadTaskMapper.java
@@ -0,0 +1,7 @@
+package com.bilibili.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.bilibili.entity.TbUploadTask;
+
+public interface UploadTaskMapper extends BaseMapper<TbUploadTask> {
+}
---
+++ bilibili-server/src/main/java/com/bilibili/mapper/VideoEpisodeMapper.java
@@ -0,0 +1,7 @@
+package com.bilibili.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.bilibili.entity.TbVideoEpisode;
+
+public interface VideoEpisodeMapper extends BaseMapper<TbVideoEpisode> {
+}
